c9477f62f755b9b7ca7d10ae14431a878becaf33
Erablast <maxime50maxtime@gmail.com>
2021-05-25 12:49:20 +0200

    Route post signup WIP, problème de CORS WIP

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const User = require("./Models/user");
+const userRoutes = require("./Routes/user")
 
 const app = express();
 
@@ -10,12 +12,16 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.db9o0.mongodb.net/myFirstDa
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.status(200).json({message: "ok"})
   next();
 });
 
+app.use("/api/auth/signup", userRoutes)
+
+
 module.exports = app;
+

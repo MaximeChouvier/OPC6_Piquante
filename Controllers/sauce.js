c2f98bbaaa8ce158c94883257ebb52477c242206
unknown <maxime.chouvier@gmail.com>
2021-06-09 17:04:16 +0200

    Fix Ajout de l'Uid utilisateur

--- Controllers/sauce.js
+++ Controllers/sauce.js
@@ -2,6 +2,7 @@ const Sauce = require('../Models/sauce.js')
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const sauce = require('../Models/sauce.js');
+const { update } = require('../Models/sauce.js');
 
 exports.addSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce)
@@ -9,13 +10,14 @@ exports.addSauce = (req, res, next) => {
     ...sauceObject,
     imageUrl: `${req.protocol}://${req.get("host")}/Images/${req.file.filename}`,
     likes: 0,
-    dislikes: 0
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: []
   });
   console.log(newSauce)
   newSauce.save()
     .then(() => res.status(200).json({message: "Nouvelle sauce crée"}))
     .catch(error => res.status(400).json({error}));
-  console.log("Nouvelle sauce crée");
 };
 
 exports.getAllSauces = (req, res, next) => {
@@ -25,7 +27,6 @@ exports.getAllSauces = (req, res, next) => {
 }
 
 exports.getOneSauce = (req, res, next) => {
-  console.log("l'ID de la sauce: " + req.params.id)
   Sauce.findOne({ _id: req.params.id })
     .then(Sauce => res.status(200).json(Sauce))
     .catch(error => res.status(404).json({ error }));
@@ -75,8 +76,10 @@ exports.likeSauce = (req, res ,next) => {
   Sauce.findById(sauceId)
     .then(result => {
       let sauceObject = result;
+      const usersLiked = sauceObject.usersLiked;
+      const usersDisliked = sauceObject.usersDisliked;
       if (likeStatus == 1) {
-        likeSauce(res, sauceId, sauceObject, userLiking)
+        likeSauce(res, sauceId, sauceObject, usersLiked, userLiking)
       } else if (likeStatus == -1) {
         dislikeSauce(res, sauceObject, userLiking)
       } else if (likeStatus == 0) {
@@ -86,16 +89,15 @@ exports.likeSauce = (req, res ,next) => {
     .catch(error => res.status(400).json({error}))
 }
 
-async function likeSauce(res, sauceId, sauceObject, userLiking){
-  let update = await {likes: sauceObject.likes + 1};
-  console.log(update)
+async function likeSauce(res, sauceId, sauceObject, usersLiked, userLiking){
+  usersLiked.push(userLiking)
+  let update = await {likes: sauceObject.likes + 1, usersLiked: usersLiked};
   Sauce.findOneAndUpdate(sauceId, update)
     .then(() => res.status(200).json())
     .catch(error => res.status(400).json({ error }));
-
 }
 
-function dislikeSauce(res, likeStatus, sauceObject, userLiking){
+function dislikeSauce(res, sauceId, sauceObject, userLiking){
   const update = { dislikes: 4 };  
   Sauce.findOneAndUpdate(sauceId, update)
     .then(() => res.status(200).json())
